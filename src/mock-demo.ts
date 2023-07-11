af83ca2fd3dd83c364ac30f22b0d71e1b1167d4c
Gordon Jesso <gjesso@speedlinesolutions.com>
2023-07-11 13:32:11 -0700

    Replace uuid.v4 with Math.random in mock-demo

--- src/mock-demo.ts
+++ src/mock-demo.ts
@@ -1,10 +1,8 @@
-import { v4 as uuidV4 } from "uuid";
-
 export interface ComplexResult {
-  number: number;
-  id: string;
+    number: number;
+    id: number;
 }
 
 export function getResultWithId(input: number): ComplexResult {
-  return { number: input + 1, id: uuidV4() };
+    return { number: input + 1, id: Math.random() };
 }
